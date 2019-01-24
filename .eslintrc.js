c417b507536080279545554ca0d8d73afc147285
王淳龙 <961737123@qq.com>
2019-01-24 17:03:14 +0800

    vue1.0

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	// https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+	extends: 'standard',
+	// required to lint *.vue files
+	plugins: [
+		'html'
+	],
+	// add your custom rules here
+	'rules': {
+		// allow paren-less arrow functions 零表示忽略规则检查
+		'arrow-parens': 0,
+		// allow async-await
+		'generator-star-spacing': 0,
+		// allow debugger during development 是否允许debugger
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+		'semi': ['error', 'always'],
+		'indent': 0,
+		'space-before-function-paren': 0
+	}
+}
---
+++ build/dev-client.js
@@ -0,0 +1,9 @@
+/* eslint-disable 热更新（改完代码，立即生效） */
+require('eventsource-polyfill')
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
+
+hotClient.subscribe(function (event) {
+  if (event.action === 'reload') {
+    window.location.reload()
+  }
+})
